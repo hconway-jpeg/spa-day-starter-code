dcd2dabfb6861555cb743c0fa6490bc12d94d63a
unknown <hconway@luc.edu>
2021-03-10 20:17:26 -0600

    completed spa day validation studio with bonus

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,13 +1,26 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.*;
+
 public class User {
+
+    @NotNull(message = "Field required")
+    @NotBlank(message = "Field required")
+    @Size(min = 5, max = 15, message = "Must be between 5 and 15 characters")
     private String username;
+
+    @Email(message = "Invalid input")
+    @NotNull(message = "Should not be null")
     private String email;
+
+    @NotNull(message = "Field required")
+    @Size(min = 6, message = "Must be at least 6 characters")
     private String password;
 
-    public User() {
+    @NotNull(message = "Passwords do not match")
+    private String verifyPassword;
 
-    }
+    public User() { }
 
     public User(String username, String email, String password) {
         this();
@@ -19,15 +32,11 @@ public class User {
     public String getUsername() {
         return username;
     }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public void setUsername(String username) { this.username = username; }
 
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
@@ -35,8 +44,23 @@ public class User {
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
     }
+
+    public String getVerifyPassword() { return verifyPassword; }
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword() {
+        if (this.password != null && this.verifyPassword != null) {
+            if (!this.password.equals(this.verifyPassword)) {
+                this.verifyPassword = null;
+            }
+        }
+    }
+
 }
